c4cf0e2c886c67ce760ff9c96c2c0f294937a02a
Riudux <richifrias54@gmail.com>
2026-03-23 01:33:01 -0600

    obligatorio
    hice que sea obligatorio el poner los campos de register y login

--- mvc/app/controllers/usuarios/register.php
+++ mvc/app/controllers/usuarios/register.php
@@ -11,6 +11,11 @@
         $sqlinsert = "INSERT INTO usuarios (username, email, password) VALUES ('$username', '$email', '$password')";
 
         $result = $conn->query($sqlinsert);
+        if ($result === TRUE) {
+            echo "Registro exitoso. <a href='../../views/login.html'> Iniciar Sesión</a>";
+        } else {
+            echo "Error: " . $sqlinsert . "<br>" . $conn->error;
+        }
 
     } 
     $conn->close();
